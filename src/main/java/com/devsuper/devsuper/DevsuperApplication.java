1ae988afcc9d1a67ca04412514be529738b019c9
Allisson Bezerra <allisson.geografia@gmail.com>
2023-08-29 21:09:20 -0300

    Erro de log

---
+++ src/main/java/com/devsuper/devsuper/DevsuperApplication.java
@@ -0,0 +1,13 @@
+package com.devsuper.devsuper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsuperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsuperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuper/devsuper/DevsuperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuper.devsuper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
