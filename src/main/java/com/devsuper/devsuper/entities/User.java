d7177f4c0e3ec3d57ba64e553d52bb8dce386fe0
Allisson Bezerra <allisson.geografia@gmail.com>
2023-08-31 10:39:15 -0300

    Relacionamento Order, Enum muitos-para-um

--- src/main/java/com/devsuper/devsuper/entities/User.java
+++ src/main/java/com/devsuper/devsuper/entities/User.java
@@ -1,11 +1,14 @@
-package com.devsuper.entities;
+package com.devsuper.devsuper.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +21,11 @@ public class User {
 	private String phone;
 	private LocalDate birthDate;
 	private String password;
+	
+	// relacionamento 1 para muitos 
+	@OneToMany(mappedBy = "client" )
+	private List<Order> order = new ArrayList<>();
+	
 	
 	public User() {
 		
@@ -70,5 +78,10 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public List<Order> getOrder() {
+		return order;
+	}
+	
 	
 }
