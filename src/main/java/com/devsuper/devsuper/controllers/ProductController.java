dd91d5c4dcce0ebe3dbd88e0b38554d4de1c2bb3
Allisson Bezerra <allisson.geografia@gmail.com>
2023-09-20 09:53:42 -0300

    PUT product atualizando o objeto no DB

--- src/main/java/com/devsuper/devsuper/controllers/ProductController.java
+++ src/main/java/com/devsuper/devsuper/controllers/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -67,5 +68,11 @@ public class ProductController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto); // alem de mostrar o código 201 informará o link do obj criado
+	}
+	
+	@PutMapping(value = "/{id}") // mapeou a rota do products // buscando por id
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto ) { 
+		dto = service.update(id, dto);
+		return ResponseEntity.ok(dto);
 	}
 }
--- src/main/java/com/devsuper/devsuper/services/ProductService.java
+++ src/main/java/com/devsuper/devsuper/services/ProductService.java
@@ -15,30 +15,32 @@ import com.devsuper.devsuper.repositories.ProductRepository;
 // notação padrao identificando a camada de servico
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
+
 	// metodo para buscar um elmento do banco pelo id (product)
-	@Transactional(readOnly = true) // essa notacao é usada nesse momento para rapidez ja que nao estou salvando nada so consultando
+	@Transactional(readOnly = true) // essa notacao é usada nesse momento para rapidez ja que nao estou salvando
+									// nada so consultando
 	public ProductDTO findById(Long id) {
 		Optional<Product> result = repository.findById(id);
 		Product product = result.get();
 		ProductDTO dto = new ProductDTO(product);
 		return dto;
-		
+
 	}
-	
+
 	// metodo para retornar uma lista de elemento do banco (pproduct)
-	@Transactional(readOnly = true) // essa notacao é usada nesse momento para rapidez ja que nao estou salvando nada so consultando
+	@Transactional(readOnly = true) // essa notacao é usada nesse momento para rapidez ja que nao estou salvando
+									// nada so consultando
 	public Page<ProductDTO> findAll(Pageable pageable) {
-		Page<Product> result = repository.findAll(pageable); // page tipo especial de LIST 
+		Page<Product> result = repository.findAll(pageable); // page tipo especial de LIST
 		return result.map(x -> new ProductDTO(x)); // expressão lambda
-		
+
 	}
-	
-	//Criando o metodo de incluir e salvar no DB
-	
+
+	/*
+	// Criando o metodo de incluir e salvar no DB (Método Padrão)
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
@@ -46,12 +48,55 @@ public class ProductService {
 		entity.setDescription(dto.getDescription());
 		entity.setPrice(dto.getPrice());
 		entity.setImgUrl(dto.getImgUrl());
-		
+
 		entity = repository.save(entity); // chamando a classe repository pra salvar no DB
 		return new ProductDTO(entity);
-		
-		
-	}
-	
 
+	} */
+	
+	// Criando o metodo de incluir e salvar no DB (Método auxiliar sendo usado: boas práticas)
+		@Transactional
+		public ProductDTO insert(ProductDTO dto) {
+			Product entity = new Product();
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity); // chamando a classe repository pra salvar no DB
+			return new ProductDTO(entity);
+
+		}
+
+	/*
+	 * // UPDATE atualizando um produto (Método Padrão)
+	 * 
+	 * @Transactional public ProductDTO update(Long id, ProductDTO dto) {
+	 * 
+	 * Product entity = repository.getReferenceById(id); //instanciando um produto
+	 * com a referencia do Id informado entity.setName(dto.getName());
+	 * entity.setDescription(dto.getDescription()); entity.setPrice(dto.getPrice());
+	 * entity.setImgUrl(dto.getImgUrl());
+	 * 
+	 * entity = repository.save(entity); // chamando a classe repository pra salvar
+	 * no DB return new ProductDTO(entity); }
+	 */
+
+	
+		
+	// UpDate (Atualização de um objeto) Método com boas práticas ... Usa um metodo
+	// auxiliar para chamar os atributos
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+
+		Product entity = repository.getReferenceById(id); // instanciando um produto com a referencia do Id informado
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity); // chamando a classe repository pra salvar no DB
+		return new ProductDTO(entity);
+	}
+
+	// Método auxiliar para chamar os atributos da Entity  Product e direcionar para DTO 
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+
+	}
 }
