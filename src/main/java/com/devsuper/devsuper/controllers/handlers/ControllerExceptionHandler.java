10f04fa114b348d691d54bde1271fdccfccabd92
Allisson Bezerra <allisson.geografia@gmail.com>
2023-09-23 07:52:33 -0300

    Ajuste na classe handlers, foltava a anotation ControllAdvice

--- src/main/java/com/devsuper/devsuper/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/devsuper/devsuper/controllers/handlers/ControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.devsuper.devsuper.dto.CustomError;
@@ -11,6 +12,7 @@ import com.devsuper.devsuper.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+@ControllerAdvice
 public class ControllerExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
